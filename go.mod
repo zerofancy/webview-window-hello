1afca8111d2accc42774ae49f3989b1502b4c4de
zerofancy <ntutn.top@gmail.com>
2024-03-29 03:05:57 +0800

    First Version

---
+++ go.mod
@@ -0,0 +1,5 @@
+module hello
+
+go 1.19
+
+require github.com/webview/webview_go v0.0.0-20240220051247-56f456ca3a43
---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"embed"
+	_ "embed"
+	"io/fs"
+	"net"
+	"net/http"
+	"strconv"
+
+	webview "github.com/webview/webview_go"
+)
+
+//go:embed assets
+var f embed.FS
+
+func main() {
+	fSys, err := fs.Sub(f, "assets")
+	if err != nil {
+		panic(err)
+	}
+	http.Handle("/", http.FileServer(http.FS(fSys)))
+
+	listener, err := net.Listen("tcp", ":0")
+	if err != nil {
+		panic(err)
+	}
+	port := listener.Addr().(*net.TCPAddr).Port
+	go http.Serve(listener, nil)
+
+	w := webview.New(false)
+	defer w.Destroy()
+	w.SetTitle("Basic Example")
+	w.SetSize(1000, 618, webview.HintNone)
+	w.Navigate("http://localhost:" + strconv.Itoa(port))
+	w.Run()
+}
